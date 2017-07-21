784ba7cd59be60a060203a53a6d66c74449bd9bb
Nicholas Mead <nixter11@uw.edu>
2017-07-21 09:34:32 -0700

    finish adding new Item list to firebase character entry

--- src/app/character.model.ts
+++ src/app/character.model.ts
@@ -1,5 +1,7 @@
+import { Item } from './item.model';
+
 export class Character {
-    constructor(public name: string, public race: string, public gender: string, public characterClass: string, public strength: number, public dexterity: number, public constitution: number, public intelligence: number, public wisdom: number, public charisma: number) {
+    constructor(public name: string, public race: string, public gender: string, public characterClass: string, public strength: number, public dexterity: number, public constitution: number, public intelligence: number, public wisdom: number, public charisma: number, public equipmentList: Item[]) {
 
     }
 }
--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -25,10 +25,11 @@ export class CreateComponent implements OnInit
   }
 
   finishCreate(name: string, race: string, gender: string,  characterClass: string, newStrength: number, newDexterity: number, newConstitution: number, newIntelligence: number, newWisdom: number, newCharisma: number){
-      let newCharacter: Character = new Character(name, race, gender, characterClass, newStrength, newDexterity, newConstitution, newIntelligence, newWisdom, newCharisma);
+      let newCharacter: Character = new Character(name, race, gender, characterClass, newStrength, newDexterity, newConstitution, newIntelligence, newWisdom, newCharisma, this.addedItemList);
       console.log(newCharacter);
       this.createService.addCharacter(newCharacter);
       this.doneWithForm.emit(true);
+      this.addedItemList = [];
   }
 
   addToCreatePlayerItemList(newItem) {
